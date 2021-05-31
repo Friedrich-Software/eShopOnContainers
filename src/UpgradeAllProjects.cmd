83fc3f52da210034c451b855a215a0f0e8fb8d3a
Jürgen Friedrich <Info@Friedrich-Software.com>
2021-05-31 11:15:31 +0200

    Added UpgradAllProjects.cmd script for upgrad via upgrade-assistant

---
+++ src/UpgradeAllProjects.cmd
@@ -0,0 +1,36 @@
+
+
+upgrade-assistant ApiGateways\Mobile.Bff.Shopping\aggregator\Mobile.Shopping.HttpAggregator.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant ApiGateways\Web.Bff.Shopping\aggregator\Web.Shopping.HttpAggregator.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant BuildingBlocks\Devspaces.Support\Devspaces.Support.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant BuildingBlocks\EventBus\EventBus\EventBus.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant BuildingBlocks\EventBus\EventBus.Tests\EventBus.Tests.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant BuildingBlocks\EventBus\EventBusRabbitMQ\EventBusRabbitMQ.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant BuildingBlocks\EventBus\EventBusServiceBus\EventBusServiceBus.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant BuildingBlocks\EventBus\IntegrationEventLogEF\IntegrationEventLogEF.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant BuildingBlocks\WebHostCustomization\WebHost.Customization\WebHost.Customization.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Basket\Basket.API\Basket.API.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Basket\Basket.FunctionalTests\Basket.FunctionalTests.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Basket\Basket.UnitTests\Basket.UnitTests.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Catalog\Catalog.API\Catalog.API.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Catalog\Catalog.FunctionalTests\Catalog.FunctionalTests.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Catalog\Catalog.UnitTests\Catalog.UnitTests.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Identity\Identity.API\Identity.API.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Ordering\Ordering.API\Ordering.API.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Ordering\Ordering.BackgroundTasks\Ordering.BackgroundTasks.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Ordering\Ordering.Domain\Ordering.Domain.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Ordering\Ordering.FunctionalTests\Ordering.FunctionalTests.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Ordering\Ordering.Infrastructure\Ordering.Infrastructure.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Ordering\Ordering.SignalrHub\Ordering.SignalrHub.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Ordering\Ordering.UnitTests\Ordering.UnitTests.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Payment\Payment.API\Payment.API.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Services\Webhooks\Webhooks.API\Webhooks.API.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Tests\Services\Application.FunctionalTests\Application.FunctionalTests.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Web\WebhookClient\WebhookClient.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Web\WebMVC\WebMVC.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Web\WebSPA\WebSPA.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+upgrade-assistant Web\WebStatus\WebStatus.csproj --skip-backup --non-interactive --target-tfm-support  Preview
+
+
+
+
